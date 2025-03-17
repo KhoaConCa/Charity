986612b8f4aadc9e688148859f170f4283c39733
tuandanh <nguyentuandanhk15@siu.edu.vn>
2025-03-17 13:31:05 +0700

    done up file(photo, video) in aws3 . but not scalable

---
+++ profileService/src/main/java/com/tuandanh/profileService/service/aws3/S3Service.java
@@ -0,0 +1,52 @@
+package com.tuandanh.profileService.service.aws3;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Service
+public class S3Service {
+
+    private final S3Client s3Client;
+    private final String bucketName;
+
+    public S3Service(@Value("${aws3.access_key}") String accessKey,
+                     @Value("${aws3.secret_key}") String secretKey,
+                     @Value("${aws3.bucket_name}") String bucketName,
+                     @Value("${aws3.region}") String region) {
+
+        this.bucketName = bucketName;
+
+        AwsBasicCredentials awsCreds = AwsBasicCredentials.create(accessKey, secretKey);
+        this.s3Client = S3Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(awsCreds))
+                .build();
+    }
+
+    public String uploadAvatar(MultipartFile file) throws IOException {
+        String fileName = UUID.randomUUID().toString() + "_" + file.getOriginalFilename();
+
+        s3Client.putObject(
+                PutObjectRequest.builder()
+                        .bucket(bucketName)
+                        .key(fileName)
+                        .contentType(file.getContentType())
+                        .build(),
+                RequestBody.fromBytes(file.getBytes())
+        );
+
+
+        // Return public URL
+        return "https://" + bucketName + ".s3.amazonaws.com/" + fileName;
+    }
+}
