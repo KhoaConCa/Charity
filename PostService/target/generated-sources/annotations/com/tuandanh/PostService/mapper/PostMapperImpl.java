f2e312caa22c98544fa0ce0c7b0fdb431a62e079
KhoaDN <dnk270204@gmail.com>
2025-04-16 22:44:05 +0700

    test

--- PostService/target/generated-sources/annotations/com/tuandanh/PostService/mapper/PostMapperImpl.java
+++ PostService/target/generated-sources/annotations/com/tuandanh/PostService/mapper/PostMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 22.0.2 (Oracle Corporation)"
+    comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.42.0.z20250331-1358, environment: Java 21.0.6 (Eclipse Adoptium)"
 )
 @Component
 public class PostMapperImpl implements PostMapper {
@@ -22,20 +22,20 @@ public class PostMapperImpl implements PostMapper {
 
         PostResponse.PostResponseBuilder postResponse = PostResponse.builder();
 
-        postResponse.id( post.getId() );
-        postResponse.profileId( post.getProfileId() );
         postResponse.content( post.getContent() );
+        postResponse.createdAt( post.getCreatedAt() );
         List<String> list = post.getFileIds();
         if ( list != null ) {
             postResponse.fileIds( new ArrayList<String>( list ) );
         }
+        postResponse.id( post.getId() );
+        postResponse.point( post.getPoint() );
+        postResponse.privacy( post.getPrivacy() );
+        postResponse.profileId( post.getProfileId() );
         List<String> list1 = post.getTags();
         if ( list1 != null ) {
             postResponse.tags( new ArrayList<String>( list1 ) );
         }
-        postResponse.privacy( post.getPrivacy() );
-        postResponse.point( post.getPoint() );
-        postResponse.createdAt( post.getCreatedAt() );
         postResponse.updatedAt( post.getUpdatedAt() );
 
         return postResponse.build();
