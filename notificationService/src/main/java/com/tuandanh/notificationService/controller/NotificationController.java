33f188a6f05aa3b3a0f0513d22aee79bf346e70d
tuandanh <nguyentuandanhk15@siu.edu.vn>
2025-04-22 21:11:34 +0700

    finally??

--- notificationService/src/main/java/com/tuandanh/notificationService/controller/NotificationController.java
+++ notificationService/src/main/java/com/tuandanh/notificationService/controller/NotificationController.java
@@ -7,10 +7,7 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,6 +22,33 @@ public class NotificationController {
     @GetMapping("/{userId}")
     public ApiResponse<List<FakeNotificationLog>> getListNotificationByUser(@PathVariable String userId) {
         List<FakeNotificationLog> list = pushNotificationService.getListNotificationByUsers(userId);
+
+        return ApiResponse.<List<FakeNotificationLog>>builder()
+                .result(list)
+                .build();
+    }
+
+    @GetMapping("/unread/{userId}")
+    public ApiResponse<List<FakeNotificationLog>> getUnreadListNotificationByUser(@PathVariable String userId) {
+        var list = pushNotificationService.getUnreadNotificationLogs(userId);
+
+        return ApiResponse.<List<FakeNotificationLog>>builder()
+                .result(list)
+                .build();
+    }
+
+    @PostMapping("/markAsRead/{notificationId}")
+    public ApiResponse<FakeNotificationLog> markNotificationAsRead(@PathVariable String notificationId) {
+        var notification = pushNotificationService.markAsRead(notificationId);
+
+        return ApiResponse.<FakeNotificationLog>builder()
+                .result(notification)
+                .build();
+    }
+
+    @PostMapping("/marAllAsRead/{userId}")
+    public ApiResponse<List<FakeNotificationLog>> markAllAsRead(@PathVariable String userId) {
+        List<FakeNotificationLog> list = pushNotificationService.markAllAsRead(userId);
 
         return ApiResponse.<List<FakeNotificationLog>>builder()
                 .result(list)
--- notificationService/src/main/java/com/tuandanh/notificationService/repository/FakeNotificationLogRepository.java
+++ notificationService/src/main/java/com/tuandanh/notificationService/repository/FakeNotificationLogRepository.java
@@ -1,11 +1,11 @@
 package com.tuandanh.notificationService.repository;
 
 import com.tuandanh.notificationService.entity.FakeNotificationLog;
-import com.tuandanh.notificationService.entity.NotificationLog;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
 
 public interface FakeNotificationLogRepository extends MongoRepository<FakeNotificationLog, String> {
     List<FakeNotificationLog> findByUserId(String userId);
+    List<FakeNotificationLog> findByUserIdAndIsReadFalseOrderByCreatedAtDesc(String userId);
 }
